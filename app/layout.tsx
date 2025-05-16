e5b801eb51d582f206901cebb8ce1ba8c7bd25ba
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-05-15 21:23:33 -0600

    updated vercel metadate tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,19 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Leona Motyer - Portfolio',
-  description: 'Platform Engineer & Developer Portfolio',
+  description: 'Platform Engineer & Software Developer Portfolio',
+  openGraph: {
+    title: 'Leona Motyer - Portfolio',
+    description: 'Platform Engineer & Software Developer Portfolio',
+    type: 'website',
+    url: 'https://motyer.ca',
+    siteName: 'Leona Motyer',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Leona Motyer - Portfolio',
+    description: 'Platform Engineer & Software Developer Portfolio',
+  },
 };
 
 export default function RootLayout({
